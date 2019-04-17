068b03aff821649bde6be3e84803f87662e9631b
script01 <christianonyango92@gmail.com>
2019-04-17 13:04:17 +0300

    audio mp3

--- app/Http/Controllers/VoiceLogController.php
+++ app/Http/Controllers/VoiceLogController.php
@@ -106,7 +106,7 @@ class VoiceLogController extends Controller
         $response  = '<?xml version="1.0" encoding="UTF-8"?>';
         $response .= '<Response>';
         $response .= '<Say>Please listen to our awesome record</Say>';
-        $response .= '<Play url= "http://41.215.81.58:4500/diamond.mp3"/>';
+        $response .= '<Play url= "https://www.youtube.com/watch?v=iIno0Zqhme0&feature=youtu.be"/>';
         $response .= '</Response>';
         // Print the response onto the page so that our gateway can read it
         header('Content-type: apllication/xml');
